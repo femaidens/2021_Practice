8382dee23e0a08afd549deebc8c924fc318e2bce
Kathryn <kathrynlle9@gmail.com>
2021-03-08 17:08:20 -0500

    added kathryn

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+//Kathryn pushed here
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
